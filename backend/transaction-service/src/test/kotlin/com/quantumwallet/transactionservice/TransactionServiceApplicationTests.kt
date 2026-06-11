a6c5d7550ae6ba5a6b59039cb76239db10daf8eb
Vinicius-Infra <devopsengineer048@gmail.com>
2026-06-11 16:14:38 -0300

    feat(backend): bootstrap transaction-service with kotlin and spring boot

---
+++ backend/transaction-service/src/test/kotlin/com/quantumwallet/transactionservice/TransactionServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quantumwallet.transactionservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
