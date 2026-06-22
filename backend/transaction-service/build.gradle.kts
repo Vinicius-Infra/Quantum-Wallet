483360dd7f60b363004a35d811ae3ed95dda6abc
Vinicius-Infra <devopsengineer048@gmail.com>
2026-06-22 13:29:23 -0300

    chore(backend): clean gradle cache and successfully initialize mysql table schema

--- backend/transaction-service/build.gradle.kts
+++ backend/transaction-service/build.gradle.kts
@@ -26,10 +26,12 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
 	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	
 }
 
 kotlin {
