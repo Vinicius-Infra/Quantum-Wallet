ecde2a30cef161ee6c65569a2e439c47651309fb
Vinicius-Infra <devopsengineer048@gmail.com>
2026-06-26 18:19:27 -0300

    feat(backend): add query method to filter transactions by wallet id in repository #1

--- backend/transaction-service/src/main/kotlin/com/quantumwallet/transactionservice/repository/TransactionRepository.kt
+++ backend/transaction-service/src/main/kotlin/com/quantumwallet/transactionservice/repository/TransactionRepository.kt
@@ -1,12 +1,13 @@
-
 package com.quantumwallet.transactionservice.repository
 
 import com.quantumwallet.transactionservice.model.Transaction
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.UUID
 
+@Repository
 interface TransactionRepository : JpaRepository<Transaction, UUID> {
-    
-    // Uma função inteligente: busca o histórico de transações onde a carteira foi a de origem OU a de destino
+
+    // 🆕 O objetivo de hoje: Buscar transações onde o ID seja a origem OU o destino
     fun findBySourceWalletIdOrDestinationWalletId(sourceWalletId: UUID, destinationWalletId: UUID): List<Transaction>
 }
