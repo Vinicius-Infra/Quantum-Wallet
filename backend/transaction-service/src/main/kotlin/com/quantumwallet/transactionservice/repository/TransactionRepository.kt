6ac3dcadd770e64801b1a92000d8fda1080c5880
Vinicius-Infra <devopsengineer048@gmail.com>
2026-06-27 07:13:27 -0300

    feat(backend): remove duplicate TransactionService declaration to fix compile error #1

--- backend/transaction-service/src/main/kotlin/com/quantumwallet/transactionservice/repository/TransactionRepository.kt
+++ backend/transaction-service/src/main/kotlin/com/quantumwallet/transactionservice/repository/TransactionRepository.kt
@@ -8,6 +8,5 @@ import java.util.UUID
 @Repository
 interface TransactionRepository : JpaRepository<Transaction, UUID> {
 
-    // 🆕 O objetivo de hoje: Buscar transações onde o ID seja a origem OU o destino
     fun findBySourceWalletIdOrDestinationWalletId(sourceWalletId: UUID, destinationWalletId: UUID): List<Transaction>
 }
